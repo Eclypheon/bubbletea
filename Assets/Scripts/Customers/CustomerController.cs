471343d6fe46d05b1750c02bbcd7c247df493e84
Kester <kester.neo@gmail.com>
2026-08-30 12:49:51 +0800

    feat: require Tapioca Pearls for Landlord favorite drink bribe

--- Assets/Scripts/Customers/CustomerController.cs
+++ Assets/Scripts/Customers/CustomerController.cs
@@ -377,11 +377,13 @@ namespace BubbleTeaShop
         {
             if (!isLandlordActive) return;
 
-            // Check favorite recipe: Oolong Tea + Fresh Milk + 100% Sugar + 50% Ice
+            // Check favorite recipe: Oolong Tea + Fresh Milk + 100% Sugar + 50% Ice + Tapioca Pearls
             bool isFavorite = (cup.tea == TeaBase.OolongTea &&
                                cup.milk == MilkType.FreshMilk &&
                                cup.sweetnessPercent == 100 &&
-                               cup.icePercent == 50);
+                               cup.icePercent == 50 &&
+                               cup.toppings != null &&
+                               cup.toppings.Contains(ToppingType.TapiocaPearls));
 
             if (isFavorite)
             {
@@ -409,7 +411,7 @@ namespace BubbleTeaShop
 
                     if (speechBubble != null)
                     {
-                        speechBubble.ShowMessage("W-wait... Is this Oolong Milk Tea with 100% sugar and 50% ice?! H-how did you know this is my favorite...?! ...O-okay, fine! Just for this once, I'll lower your rent by 10%! But don't get the wrong idea, baka!");
+                        speechBubble.ShowMessage("W-wait... Is this Oolong Milk Tea with tapioca pearls, 100% sugar and 50% ice?! H-how did you know this is my favorite...?! ...O-okay, fine! Just for this once, I'll lower your rent by 10%! But don't get the wrong idea, baka!");
                     }
                     HUDController.Instance?.ShowNotification("💖 Landlord loved her favorite drink! Rent reduced by 10%!", 4f);
                 }
